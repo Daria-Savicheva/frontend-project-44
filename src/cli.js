aff47ac2da1f98927b3b2e756f5512edd36f50a7
Daria Savicheva <savicheva.darya@hexly.ru>
2023-02-02 09:58:27 +0300

    step 3, eslinter

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-console */
 import readlineSync from 'readline-sync';
 
-export default function userName(){
-    console.log('brain-games');
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ')
-    console.log(`Hello, ${name}!`)
-};
+export default function userName() {
+  console.log('brain-games');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+}
