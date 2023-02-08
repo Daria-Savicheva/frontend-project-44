ce068fa088c23ce093ce29238e4524f0904b6949
Daria Savicheva <savicheva.darya@hexly.ru>
2023-02-08 22:42:59 +0300

    add link asccinema in README.md

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import { welcomeUser, randomNum } from '../src/cli.js';
 
--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,6 @@
 import readlineSync from 'readline-sync';
 
 export function welcomeUser() {
-  console.log('brain-games');
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
