17068e9bd06d74184b5da7ee2932d243752d7bc6
Daria Savicheva <savicheva.darya@hexly.ru>
2023-02-08 22:18:47 +0300

    add brain-even.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-console */
 import readlineSync from 'readline-sync';
 
-export default function userName() {
+export function welcomeUser() {
   console.log('brain-games');
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
+}
+
+export function randomNum(min, max) {
+  const x = Math.floor(Math.random() * (max - min + 1) + min);
+  return x;
 }
